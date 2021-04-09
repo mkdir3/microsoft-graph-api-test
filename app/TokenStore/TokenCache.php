2d450917b39ae3fdb7793f76248bcf14f1fcb9c2
Ted <p0599847@protonmail.com>
2021-04-09 15:43:40 +0200

    Update Token

--- app/TokenStore/TokenCache.php
+++ app/TokenStore/TokenCache.php
@@ -3,7 +3,6 @@
 namespace App\TokenStore;
 
 class TokenCache {
-    
   public function storeTokens($accessToken, $user) {
     session([
       'accessToken' => $accessToken->getToken(),
@@ -24,6 +23,7 @@ class TokenCache {
     session()->forget('userTimeZone');
   }
 
+  // <GetAccessTokenSnippet>
   public function getAccessToken() {
     // Check if tokens exist
     if (empty(session('accessToken')) ||
@@ -32,6 +32,51 @@ class TokenCache {
       return '';
     }
 
+    // Check if token is expired
+    //Get current time + 5 minutes (to allow for time differences)
+    $now = time() + 300;
+    if (session('tokenExpires') <= $now) {
+      // Token is expired (or very close to it)
+      // so let's refresh
+
+      // Initialize the OAuth client
+      $oauthClient = new \League\OAuth2\Client\Provider\GenericProvider([
+        'clientId'                => config('azure.appId'),
+        'clientSecret'            => config('azure.appSecret'),
+        'redirectUri'             => config('azure.redirectUri'),
+        'urlAuthorize'            => config('azure.authority').config('azure.authorizeEndpoint'),
+        'urlAccessToken'          => config('azure.authority').config('azure.tokenEndpoint'),
+        'urlResourceOwnerDetails' => '',
+        'scopes'                  => config('azure.scopes')
+      ]);
+
+      try {
+        $newToken = $oauthClient->getAccessToken('refresh_token', [
+          'refresh_token' => session('refreshToken')
+        ]);
+
+        // Store the new values
+        $this->updateTokens($newToken);
+
+        return $newToken->getToken();
+      }
+      catch (\League\OAuth2\Client\Provider\Exception\IdentityProviderException $e) {
+        return '';
+      }
+    }
+
+    // Token is still valid, just return it
     return session('accessToken');
   }
+  // </GetAccessTokenSnippet>
+
+  // <UpdateTokensSnippet>
+  public function updateTokens($accessToken) {
+    session([
+      'accessToken' => $accessToken->getToken(),
+      'refreshToken' => $accessToken->getRefreshToken(),
+      'tokenExpires' => $accessToken->getExpires()
+    ]);
+  }
+  // </UpdateTokensSnippet>
 }
