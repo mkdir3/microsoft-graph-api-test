d930ef4324627b230952ab81e3f8fd6800027b3f
Ted <p0599847@protonmail.com>
2021-04-09 14:29:04 +0200

    front layout
    requiring oauth2-client & microsoft graph

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,26 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    // <LoadViewDataSnippet>
+    public function loadViewData()
+    {
+        $viewData = [];
+
+        // Check for flash errors
+        if (session('error')) {
+            $viewData['error'] = session('error');
+            $viewData['errorDetail'] = session('errorDetail');
+        }
+
+        // Check for logged on user
+        if (session('userName')) {
+            $viewData['userName'] = session('userName');
+            $viewData['userEmail'] = session('userEmail');
+            $viewData['userTimeZone'] = session('userTimeZone');
+        }
+
+        return $viewData;
+    }
+    // </LoadViewDataSnippet>
 }
